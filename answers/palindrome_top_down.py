58d0a6fa32126ad2adbf6ba9cb717c5662020535
Hendrikto <hendrik.to@gmail.com>
2016-11-21 22:36:08 +0100

    Add code for 9.3 a.

---
+++ answers/palindrome_top_down.py
@@ -0,0 +1,23 @@
+# author: Hendrik Werner s4549775
+
+def longest_palindrome(word: str, i: int, j: int) -> int:
+    if i > j:
+        return 0
+    left = i
+    right = j
+    longest_palindromes = [1]
+    while left < right:
+        if word[left] == word[right]:
+            longest_palindromes.append(2 + longest_palindrome(word, left + 1, right - 1))
+        right -= 1
+    right = j
+    while left < right:
+        if word[left] == word[right]:
+            longest_palindromes.append(2 + longest_palindrome(word, left + 1, right - 1))
+        left += 1
+    return max(longest_palindromes)
+
+
+if __name__ == "__main__":
+    word = "character"
+    print(longest_palindrome(word, 0, len(word) - 1))
