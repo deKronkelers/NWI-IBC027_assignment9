13e13faeb5e6c4aeb089012f71a695ddd01e1f7f
Hendrikto <hendrik.to@gmail.com>
2016-11-22 22:13:45 +0100

    Add code for 9.2 a.

---
+++ answers/code/findCPathsRecursive.groovy
@@ -0,0 +1,29 @@
+/**
+ * @author Hendrik Werner s4549775
+ *
+ * @param matrix M from the exercise
+ * @param c C from the exercise
+ * @param m end of C-path
+ * @param n end of C-path
+ * @param x current x position
+ * @param y current y position
+ * @param value the value we accumulated up to this point
+ * @return the number of C-paths from x, y to m, n
+ */
+int findCPaths(
+        int[][] matrix, int c, int m, int n,
+        int x = 0, int y = 0, int value = 0
+) {
+    if (value > c || x > m || y > n) {
+        return 0
+    }
+    int currentValue = value + matrix[x][y]
+    if (x == m && y == n && currentValue == c) {
+        return 1
+    }
+    findCPaths(matrix, c, m, n, x + 1, y, currentValue) +
+            findCPaths(matrix, c, n, n, x, y + 1, currentValue)
+}
+
+int[][] matrix = [[1, 2, 3], [4, 6, 5], [3, 2, 1]]
+println findCPaths(matrix, 12, 2, 2)
